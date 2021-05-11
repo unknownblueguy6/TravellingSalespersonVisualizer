89e968804237864e4d2eea2d7e48b801e6d2cff2
unknownblueguy6 <unknownblueguy6@gmail.com>
2021-05-11 18:41:57 +0530

    Fix drawPath() float error

--- gfx.py
+++ gfx.py
@@ -29,8 +29,8 @@ def drawPath(surface, cities, order):
     if len(order) != len(cities):
         return
     r = CITY_SIZE // len(cities)
-    r = max(MIN_CITY_SIZE, r)
-    r = min(MAX_CITY_SIZE, r)
+    r = int(max(MIN_CITY_SIZE, r))
+    r = int(min(MAX_CITY_SIZE, r))
     order = order[order.index(0):] + order[:order.index(0)]
     for i in range(len(order)):
         cityA = cities[order[i % len(order)]]
